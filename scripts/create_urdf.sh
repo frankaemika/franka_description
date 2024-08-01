7b87c68b557ee9fd6790e84438593bf8bf2c8a45
Daniel Larby <dan_larby@hotmail.co.uk>
2024-08-01 13:40:47 +0100

    Added quotations around path in create_franka.sh to support paths with spaces in.

--- scripts/create_urdf.sh
+++ scripts/create_urdf.sh
@@ -5,7 +5,8 @@ docker build -t urdf_creation ./.docker
 echo 
 
 docker run -u 1001 \
-    -v $(pwd):/workspaces/src/franka_description \
+    -v "$(pwd):/workspaces/src/franka_description" \
     -w /workspaces/src/franka_description \
     urdf_creation \
     .docker/create_urdf.entrypoint.sh $*
+
