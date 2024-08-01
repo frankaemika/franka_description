76a21714fd9562732b4dd7903711ba9762eef8cd
Daniel Larby <dan_larby@hotmail.co.uk>
2024-08-01 13:57:59 +0100

    Added default user and group to fix permissions issues when trying to create folders/files.

--- scripts/create_urdf.sh
+++ scripts/create_urdf.sh
@@ -7,6 +7,7 @@ echo
 docker run -u 1001 \
     -v "$(pwd):/workspaces/src/franka_description" \
     -w /workspaces/src/franka_description \
+    --user "$(id -u):$(id -g)" \
     urdf_creation \
     .docker/create_urdf.entrypoint.sh $*
 
