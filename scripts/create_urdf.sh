8a3e6db53895a9c7ef50322d23d1b802ab3a01fe
Simone Silenzi <simone.silenzi@franka.de>
2024-11-20 09:47:41 +0000

    fix: scripts create_urdf.sh and visualize_franka.sh need the correct user id
    * commit '2525a2ebf556dc8326f1479d4c47755df64db271':
      fix: correct same problem on visualize_franka.sh
      fix: script create_urdf.sh needs the correct user id

--- scripts/create_urdf.sh
+++ scripts/create_urdf.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-docker build -t urdf_creation ./.docker
+docker build -t urdf_creation \
+    --build-arg USER_UID=$(id -u) \
+    --build-arg USER_GID=$(id -g) \
+    ./.docker
 
 echo 
 
-docker run -u 1001 \
+docker run -u $(id -u) \
     -v $(pwd):/workspaces/src/franka_description \
     -w /workspaces/src/franka_description \
     urdf_creation \
--- scripts/visualize_franka.sh
+++ scripts/visualize_franka.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-docker build -t urdf_creation ./.docker
+docker build -t urdf_creation \
+    --build-arg USER_UID=$(id -u) \
+    --build-arg USER_GID=$(id -g) \
+    ./.docker
 
 echo 
 
-docker run -it -u 1001 \
+docker run -it -u $(id -u) \
     --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=${DISPLAY} \
     -v $(pwd):/workspaces/src/franka_description \
